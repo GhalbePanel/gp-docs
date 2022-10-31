4752193091431eec8cba11bc31978d038d90ef77
Arash <97286379+Ghalbeyou@users.noreply.github.com>
2022-10-31 21:45:41 +0330

    Some editing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,50 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>GhalbePanel Document V1</title>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://github.com/GhalbePanel">GhalbePanel</a> Document
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          In this document you will learn how to work and start with <a href='#'>GhalbePanel</a>.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link legacyBehavior href={"/v1/gettingstarted"}>
+          <a className={styles.card}>
+            <h2>Getting Started &rarr;</h2>
+            <p>Start building your panel with GhalbePanel, today with easy learning!</p>
+          </a></Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://github.com/GhalbePanel/GhalbePanel" className={styles.card}>
+            <h2>GitHub &rarr;</h2>
+            <p>This project is 100% open source, its avaible on github, for everyone!</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
+          Template by{' '}
+          <span className={styles.logo}>
+          <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </a>
           </span>
-        </a>
+          &rarr; and other copyrights for GhalbePanel
       </footer>
     </div>
   )
